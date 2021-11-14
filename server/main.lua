98cf1ed0e55ad37262b85eff972941adb3eb274a
vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-11-14 08:42:43 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -6,8 +6,7 @@ QBCore.Functions.CreateUseableItem("tunerlaptop", function(source, item)
     TriggerClientEvent('qb-tunerchip:client:openChip', source)
 end)
 
-RegisterServerEvent('qb-tunerchip:server:TuneStatus')
-AddEventHandler('qb-tunerchip:server:TuneStatus', function(plate, bool)
+RegisterNetEvent('qb-tunerchip:server:TuneStatus', function(plate, bool)
     if bool then
         tunedVehicles[plate] = bool
     else
@@ -36,8 +35,7 @@ QBCore.Functions.CreateUseableItem("nitrous", function(source, item)
     TriggerClientEvent('smallresource:client:LoadNitrous', source)
 end)
 
-RegisterServerEvent('nitrous:server:LoadNitrous')
-AddEventHandler('nitrous:server:LoadNitrous', function(Plate)
+RegisterNetEvent('nitrous:server:LoadNitrous', function(Plate)
     VehicleNitrous[Plate] = {
         hasnitro = true,
         level = 100,
@@ -45,23 +43,20 @@ AddEventHandler('nitrous:server:LoadNitrous', function(Plate)
     TriggerClientEvent('nitrous:client:LoadNitrous', -1, Plate)
 end)
 
-RegisterServerEvent('nitrous:server:SyncFlames')
-AddEventHandler('nitrous:server:SyncFlames', function(netId)
+RegisterNetEvent('nitrous:server:SyncFlames', function(netId)
     TriggerClientEvent('nitrous:client:SyncFlames', -1, netId, source)
 end)
 
-RegisterServerEvent('nitrous:server:UnloadNitrous')
-AddEventHandler('nitrous:server:UnloadNitrous', function(Plate)
+RegisterNetEvent('nitrous:server:UnloadNitrous', function(Plate)
     VehicleNitrous[Plate] = nil
     TriggerClientEvent('nitrous:client:UnloadNitrous', -1, Plate)
 end)
-RegisterServerEvent('nitrous:server:UpdateNitroLevel')
-AddEventHandler('nitrous:server:UpdateNitroLevel', function(Plate, level)
+
+RegisterNetEvent('nitrous:server:UpdateNitroLevel', function(Plate, level)
     VehicleNitrous[Plate].level = level
     TriggerClientEvent('nitrous:client:UpdateNitroLevel', -1, Plate, level)
 end)
 
-RegisterServerEvent('nitrous:server:StopSync')
-AddEventHandler('nitrous:server:StopSync', function(plate)
+RegisterNetEvent('nitrous:server:StopSync', function(plate)
     TriggerClientEvent('nitrous:client:StopSync', -1, plate)
 end)
