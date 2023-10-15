940443133bc29ff92376a1e92518432beb5e8ab7
stepanvalic <valicekstepan8@seznam.cz>
2023-10-15 07:32:43 +0200

    fix: Czech translation created

--- locales/cs.lua
+++ locales/cs.lua
@@ -1,20 +1,20 @@
 local Translations = {
     success = {
-        this_vehicle_has_been_tuned = "Dieses Fahrzeug wurde getunt",
+        this_vehicle_has_been_tuned = "Toto vozidlo bylo vyladěno",
     },
     text = {
-        this_is_not_the_idea_is_it = "Ist das nicht die Idee?",
-        connecting_nos = "Verbindung zu NOS wird hergestellt...",
+        this_is_not_the_idea_is_it = "Tohle není ten nápad, že ne?",
+        connecting_nos = "Připojuji NOS...",
     },
     error = {
-        tunerchip_vehicle_tuned = "TunerChip v1.05: Fahrzeug getunt!",
-        this_vehicle_has_not_been_tuned = "Dieses Fahrzeug wurde nicht getunt",
-        no_vehicle_nearby = "Kein Fahrzeug in der Nähe",
-        tunerchip_vehicle_has_been_reset = "TunerChip v1.05: Fahrzeug wurde zurückgesetzt!",
-        you_are_not_in_a_vehicle = "Du befindest dich nicht in einem Fahrzeug",
-        you_cannot_do_that_from_this_seat = "Du kannst das nicht von diesem Sitz aus tun!",
-        you_already_have_nos_active = "Du hast bereits aktives NOS",
-        canceled = "Abgebrochen",
+        tunerchip_vehicle_tuned = "TunerChip v1.05: Vozidlo bylo vyladěno!",
+        this_vehicle_has_not_been_tuned = "Toto vozidlo nebylo vyladěno",
+        no_vehicle_nearby = "Žádné vozidlo není v blízkosti",
+        tunerchip_vehicle_has_been_reset = "TunerChip v1.05: Vozidlo bylo resetováno!",
+        you_are_not_in_a_vehicle = "Nejste ve vozidle",
+        you_cannot_do_that_from_this_seat = "Tohle nemůžete udělat z tohoto místa!",
+        you_already_have_nos_active = "Máte již aktivní NOS",
+        canceled = "Zrušeno",
     },
 }
 
